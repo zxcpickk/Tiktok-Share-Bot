335cbb7a1b4bda70556d5722e750ac59989e01b1
effyxt <88086681+effyxt@users.noreply.github.com>
2022-04-12 20:46:52 +0200

    Add files via upload

---
+++ tiktok-share-bot.py
@@ -0,0 +1,11 @@
+#https://noelp-backend.xyz/shares?id=
+#credits to Cragus and NoelP / Site   https://noelp.live/
+
+import requests
+import webbrowser
+x = input("Whats youre video id? ")
+print("example: @user/video/ID")
+webbrowser.open("https://noelp-backend.xyz/shares?id=" + x)
+
+
+# dont skid my epic code niqqaas
